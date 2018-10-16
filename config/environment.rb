25230070c4b9a9d73eaa11439fb50a62de3227d5
Manny Pissanos <mpissano@gmail.com>
2018-10-16 14:33:02 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -7,15 +7,7 @@ require 'rspec'
 
 #This is used for name spacing.
 module Concerns::Findable
-  
-    def find_by_name(name)
-      self.all.detect {|song| song.name == name}
-    end
-   
-    def find_or_create_by_name(name)
-      find_by_name(name) || create(name)
-    end
-  end
+ end
 
 
 #This is used for requiring files within our project.
