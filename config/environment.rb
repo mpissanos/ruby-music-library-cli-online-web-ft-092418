60319999de8a7d00e8a13242d0e93653eccb4fe5
Manny Pissanos <mpissano@gmail.com>
2018-10-16 14:24:58 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,23 @@
-require 'bundler'
-Bundler.require
 
+#This Is Yuour Environment File
+#Require gems here.
+require "bundler"
+require 'rspec'
+
+
+#This is used for name spacing.
 module Concerns
+  module Findable
+    def find_by_name(name)
+      self.all.detect {|song| song.name == name}
+    end
+   
+    def find_or_create_by_name(name)
+      find_by_name(name) || create(name)
+    end
+  end
 end
 
+#This is used for requiring files within our project.
+# require_relative "Other Files"
 require_all 'lib'
